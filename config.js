0f9cc731187a4608212e0b3951d6edb27a8a4165
zistks <zist0207@gmail.com>
2026-05-04 13:35:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
+    localStoragePrefix: "erikimatrata",
     title: "Party Rank Sorter",
     description: "Party rank sorter for your custom list of songs."
 };
